8034bfed499537cc9e2bd772527abe2704454f7a
monk81 <monk8@qq.com>
2017-08-22 17:00:25 +0800

    lesson06

--- APIProject/controllers/users.js
+++ APIProject/controllers/users.js
@@ -9,6 +9,26 @@ module.exports = {
         const newUser = new User(req.body);
         const user = await newUser.save();
         res.status(200).json(user);
+    },
+    getUser: async (req, res, next) => {
+        // enforce that req.body must contain all the fields
+        console.log('req.params', req.params);
+        const { userId } = req.params;
+        const users = await User.findById(userId);
+        res.status(200).json(users);
+    },
+    replaceUser: async (req,res,next) =>{
+        // req.body may contain any number of fields
+        const { userId } = req.params;
+        const newUser = req.body;
+        const result = await User.findByIdAndUpdate(userId,newUser);
+        res.status(200).json({success:true});
+    },
+    updateUser: async (req,res,next) =>{
+        const { userId } = req.params;
+        const newUser = req.body;
+        const result = await User.findByIdAndUpdate(userId,newUser);
+        res.status(200).json({success:true});
     }
 }
 /*
--- APIProject/routers/users.js
+++ APIProject/routers/users.js
@@ -5,6 +5,12 @@ const userController = require('../controllers/users');
 router.route('/')
     .get(userController.index)
     .post(userController.newUser);
+router.route('/:userId')
+    .get(userController.getUser)
+    .put(userController.replaceUser)
+    .patch(userController.updateUser)
+    ;
+
 module.exports = router;
 
 /*
